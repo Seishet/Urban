fdcbc8040b824a50b3db478e7fd3ef0a4ea33132
Seishet <seishet@gmail.com>
2024-06-12 00:16:44 +0300

    Вторая версия

--- M3/M3-P1-functions.py
+++ M3/M3-P1-functions.py
@@ -1,8 +1,9 @@
 def single_root_words(root_word, *other_words):
     same_words = []
-    root_word.lower()
+    root = root_word.lower()
     for i in range(len(other_words)):
-        if root_word in other_words[i].lower() or other_words[i].lower() in root_word:
+        word = other_words[i].lower()
+        if root.count(word) > 0 or word.count(root) > 0:
             same_words.append(other_words[i])
     return same_words
 print(single_root_words('disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
